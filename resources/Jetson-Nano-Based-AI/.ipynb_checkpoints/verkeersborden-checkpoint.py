42be2410e0fcdfe40a0305171a518ef57a827e5a
Loic Schillings <12201682@student.pxl.be>
2026-01-11 22:54:33 +0100

    Added files and updated README
    File structure change will follow

---
+++ resources/Jetson-Nano-Based-AI/.ipynb_checkpoints/verkeersborden-checkpoint.py
@@ -0,0 +1,36 @@
+from edge_impulse_linux.image import ImageImpulseRunner
+import cv2
+
+model_path = "/home/jetsonnanoo/SmartSystemsBitchhhh/inebdg-project-1-linux-aarch64-v1.eim"
+
+runner = ImageImpulseRunner(model_path)
+runner.init()
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+print(cap.isOpened())
+      
+try:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        features, _ = runner.get_features_from_image(frame)
+        result = runner.classify(features)
+
+        preds = result["result"]["classification"]
+        if preds:
+            label = max(preds, key=preds.get)
+            confidence = preds[label]
+
+            if confidence > 0.7:
+                print(f"{label} ({confidence:.2f})")
+
+except KeyboardInterrupt:
+    print("Stopped by user")
+
+finally:
+    cap.release()
+    runner.stop()
